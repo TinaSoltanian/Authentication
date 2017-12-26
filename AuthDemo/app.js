779552b3f5d6d03d1006bc5d575fcee79f1d27f4
EC2 Default User <ec2-user@ip-172-31-23-157.ec2.internal>
2017-12-26 03:05:29 +0000

    add signout and midleware fr secret page in prder to redirect user to login page if it is logged in

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -32,7 +32,7 @@ app.get("/",function(req, res){
     res.render("home");
 });
 
-app.get("/secret", function(req,res){
+app.get("/secret",isLoggedIn, function(req,res){
     res.render("secret");
 })
 
@@ -64,8 +64,20 @@ app.post("/login", passport.authenticate("local", {
     successRedirect: "/secret",
     failureRedirect: "/login"
 }), function(req, res){
-    
 });
+
+app.get("/logout", function(req, res) {
+   req.logout();
+   res.redirect("/")
+});
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started!");
