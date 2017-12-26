2baa729af1b313889000f474be38de65fcb0ec63
EC2 Default User <ec2-user@ip-172-31-23-157.ec2.internal>
2017-12-26 01:50:55 +0000

    set up required setting and use them in app, now ready to use Auth

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,9 +1,26 @@
-var express = require("express");
-var app = express();
-var mongoose = require("mongoose");
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParses              = require("body-parser"),
+    localStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose"),
+    user                    = require("./models/user");
+   
+var app = express();   
 
-mongoose.connect("mongodb://localhost/auth_demo_app", {useMongoClient: true});
+mongoose.connect("mongodb://localhost/auth_demo_app", {useMongoClient: true});   
+app.use(require("express-session")({
+   secret: "this is secret!",
+   resave: false,
+   saveUninitialized: false
+}));
+
 app.set("view engine","ejs");
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
 
 app.get("/",function(req, res){
     res.render("home");
